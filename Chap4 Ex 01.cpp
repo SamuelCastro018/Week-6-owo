19679bbb4ff6a420df5758d8af4ce1d4212000c5
SamuelCastro018 <2009029@upy.edu.mx>
2021-06-22 16:19:30 -0500

    Chap

---
+++ Chap4 Ex 01.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(){ 
+
+	printf("Name: Samuel Alberto Castro Echeverría \n");
+	printf("Social Security number:5717014111 \n");
+	printf("Date of birth: October 14, 2001\n");
+	return 0;
+	
+}
---
+++ Chap4 Ex 04.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	printf("U     U W        W      W U     U      \n");
+	printf("U     U W       W W     W U     U      \n");
+	printf("U     U W      W   W    W U     U      \n");
+	printf("U     U W     W     W   W U     U      \n");
+	printf("U     U W    W       W  W U     U      \n");
+	printf("U     U W   W          WW U     U      \n");
+	printf("UUUUUUU WWWW          WWW UUUUUUU    \n");
+
+	return(0);
+}
